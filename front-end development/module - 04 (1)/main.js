79e96cec3760633ac807515d5d48e6ee6438a694
rmdabunesar <abunesar13@gmail.com>
2025-03-16 15:26:07 +0600

    JavaScript Basics

---
+++ front-end development/module - 04 (1)/main.js
@@ -0,0 +1,74 @@
+/** 'hello' => string
+ * 
+ * 
+ *  
+*/ 
+
+// let data = `hello`;
+// console.log(typeof(data))
+
+/** 
+ * Arithmetic +, -, *, /, %, ++, --, **
+*/
+// let a = 5;
+// let b = 3;
+// console.log('a + b =', a + b)
+// console.log('a - b =', a - b)
+// console.log('a * b =', a * b)
+// console.log('a / b =', a / b)
+// console.log('a % b =', a % b)
+// console.log('a**b =', a ** b)
+// console.log('++a =', ++a)
+// console.log('--b =', --b)
+
+
+/**
+ * hello
+ * world
+ * not
+ */
+
+
+
+
+
+
+
+
+
+
+// let num1 = 20;
+// let num2 = 10;
+
+// if (num1 < num2) {
+//     console.log('condition is true');
+// } else  {
+//     console.log('condition is not true');
+    
+// }
+
+// let mark = 200
+
+// if (mark > 40 && mark < 100 && mark > 0) {
+//     console.log('pass');
+    
+// } else if (mark < 40 && mark < 100 && mark > 0) {
+//     console.log('fail');
+// } else {
+//     console.log('invalide mark');
+    
+// }
+
+
+// let mark = 30;
+
+// if(mark>=40) {
+//     if (mark >= 80) {
+//         console.log("Distinction");
+//     }
+//     else {
+//         console.log("Passed");
+//     }
+// } else {
+//     console.log("Failed");
+// }
