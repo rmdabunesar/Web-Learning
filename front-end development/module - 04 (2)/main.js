1056b43616a80352241c0481be001f2f73feeda0
rmdabunesar <abunesar13@gmail.com>
2025-03-23 16:36:05 +0600

    JavaScript Basics

---
+++ front-end development/module - 04 (2)/main.js
@@ -0,0 +1,168 @@
+// for (initialExpression; condition; updateExpression) {
+//     // loop body
+// }
+
+// for (let i = 0; i < 5; i++) {
+
+//     console.log(i);
+
+
+// }
+
+
+
+
+
+
+
+
+// initialExpression
+
+// while (condition) {
+//     // body of loop
+
+//     // updateExpression
+// }
+
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+
+//     i += 2;
+// }
+
+
+
+
+
+
+// initialExpression
+
+// do {
+//     // body of loop
+//     // updateExpression
+// } while(condition);
+
+
+// let i = 0
+// do {
+//     console.log(i);
+//     i++
+    
+// } while (i < 5);
+
+
+
+
+
+
+
+
+
+
+// let i = 6;
+// while (i<=5) {
+//     console.log(i);
+//     i++
+// }
+
+// let j = 6
+// do {
+//     console.log(j);
+//     j++
+    
+// } while (j<=5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// switch (expression) {
+//     case value1:
+        
+//         break;
+
+//     case value2:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// let rating = 4;
+
+// if (rating <= 2) {
+//     console.log("Bad rating");
+// }
+// else if (rating >= 4) {
+//     console.log("Good rating!");
+// }
+// else {
+//     console.log("Average rating");
+// }
+
+
+// let rating = 3;
+
+// switch (rating) {
+//     case 4:
+//         console.log("Good rating!");
+//         break;
+//     case 2:
+//         console.log("Bad rating!");
+//         break;
+
+//     default:
+//         console.log("Average rating");
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+// function name(params) {
+//     // code
+// }
+// name()
+
+
+
+// function showName(){
+//     console.log('Name: John');
+// }
+// showName(); 
+
+
+
+// function showName(naam){
+//     console.log('Name: ', naam);
+// }
+
+// showName('John');
+// showName('Deo');
+
+
+// function addNumber(num1, num2) {
+//     let result = num1 + num2; // 2 + 3 = 5
+//     console.log(result);
+// }
+// addNumber(2, 3);
+// addNumber(4, 5);
