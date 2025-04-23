951c458111cc6b883a5840fd01607d0bf8cc4ff1
rmdabunesar <abunesar13@gmail.com>
2025-04-23 23:57:34 +0600

    jQuery

---
+++ front-end development/module - 06 (1)/custom.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+    // $(selector).action();
+    // $("#myHeader").css("color", "blue");
+
+
+
+    // $("#myHeader").hide();
+
+    $("#myPera").mouseenter(function () {
+        alert("You entered p1!");
+    });
+
+
+
+})(jQuery)
+
+
